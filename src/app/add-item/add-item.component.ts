1834734adb290213372195043f9422f1960e8ba8
kazuya180 <hone214.oe1@gmail.com>
2024-01-18 01:35:01 +0900

    20240118　色々実施 動作未確認 ngSubmitでgoogle.comへいきたい 入力項目とリンクさせたい（itemNameやitemCode）

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
@@ -6,60 +6,80 @@ import {MatInputModule} from '@angular/material/input';
 import { HttpClient } from '@angular/common/http';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
-import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormBuilder,FormControl,FormGroup,ReactiveFormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-item',
   standalone: true,
-  imports: [CommonModule,MatFormFieldModule,MatIconModule,MatInputModule,MatButtonModule, MatDividerModule,FormBuilder],
+  imports: [CommonModule
+    ,MatFormFieldModule
+    ,MatIconModule
+    ,MatInputModule
+    ,MatButtonModule
+    ,MatDividerModule
+    ,ReactiveFormsModule
+  ],
   template: `
 
-  <form [formGroup]="" ngSubmit="Submit()">
     <div class="form-main">
-      <p>
-        <mat-form-field appearance="fill">
-        <mat-label>ItemName</mat-label>
-        <input matInput>
-        <mat-hint>Hint</mat-hint>
-        </mat-form-field>
-      </p>
-      <p>
-        <mat-form-field appearance="fill">
-        <mat-label>ItemPrice</mat-label>
-        <input matInput>
-        <mat-hint>Hint</mat-hint>
-        </mat-form-field>
-      </p>
-      <p>
-        <mat-form-field appearance="fill">
-        <mat-label>ItemCode</mat-label>
-        <input matInput>
-        <mat-hint>Hint</mat-hint>
-        </mat-form-field>
-      </p>
-      <input type="file" class="file-input"
-       (change)="onFileSelected($event)" #fileUpload>
+      <form [formGroup]="form" (ngSubmit)="onSubmit()">
+        <p>
+          <mat-form-field appearance="fill">
+          <mat-label>ItemName</mat-label>
+          <input matInput formControlName="ItemName">
+          <mat-hint>Hint</mat-hint>
+          </mat-form-field>
+        </p>
+        <p>
+          <mat-form-field appearance="fill">
+          <mat-label>ItemPrice</mat-label>
+          <input matInput formControlName="ItemPrice">
+          <mat-hint>Hint</mat-hint>
+          </mat-form-field>
+        </p>
+        <p>
+          <mat-form-field appearance="fill">
+          <mat-label>ItemCode</mat-label>
+          <input matInput formControlName="ItemCode">
+          <mat-hint>Hint</mat-hint>
+          </mat-form-field>
+        </p>
+        <input type="file" class="file-input"
+        (change)="onFileSelected($event)" #fileUpload>
 
-        <div class="file-upload">
+          <div class="file-upload">
 
-            {{fileName || "No file uploaded yet."}}
+              {{fileName || "No file uploaded yet."}}
 
-        <button mat-mini-fab color="primary" class="upload-btn"
-          (click)="fileUpload.click()">
-            <mat-icon>attach_file</mat-icon>
-          </button>
-        </div>
-        <div class='form-submit'>
-          <button mat-raised-button>Submit</button>
-        </div>
-      </div>
-    <section>
-</form>
+          <button mat-mini-fab color="primary" class="upload-btn"
+            (click)="fileUpload.click()">
+              <mat-icon>attach_file</mat-icon>
+            </button>
+          </div>
+          <!-- (click)属性を付けたが機能しない -->
+          <div class='form-submit'>
+            <button mat-raised-button>Submit</button>
+          </div>
+
+          <!-- <div class='form-submit'>
+            <button mat-raised-button (click)="onSubmit()">Submit</button>
+          </div> -->
+
+      </form>
+    </div>
+
   `,
   styleUrls: ['./add-item.component.css']
 })
-export class AddItemComponent {
+export class AddItemComponent implements OnInit{
+  form!:FormGroup;
 
+  constructor(
+    private formBuilder:FormBuilder,
+    private router: Router
+    ){};
     fileName = '';
 
     //constructor(private http: HttpClient) {}
@@ -84,15 +104,15 @@ export class AddItemComponent {
 
         }
     }
-    Submit(){
-      //
+
+    ngOnInit():void{
+      this.form = this.formBuilder.group({
+          ItemName : new FormControl(''),
+          ItemPrice : [''],
+      })
     }
 
-
-}
-export class Submit{
-  constructor(){
-      //未実装
-      //submitを押したら
-  };
+    onSubmit(){
+      this.router.navigate(['https://www.google.com']);
+    }
 }
